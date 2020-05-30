27d49e8315fb2a8b00dab3aa45f8fb27d640bb96
arm-band <bre4tn.let@gmail.com>
2020-05-30 17:53:02 +0900

    first commit.

---
+++ gulp/dir.js
@@ -0,0 +1,13 @@
+//path difinition
+module.exports = {
+    src: {
+        ejs        : './src/ejs',
+        scss       : './src/scss',
+        js         : './src/js'
+    },
+    dist: {
+        html       : './docs',
+        css        : './docs/css',
+        js         : './docs/js'
+    }
+};
